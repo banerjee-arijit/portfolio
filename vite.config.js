046271954d7c242385bb693c1812377280e5c0e1
Arijit Banerjee <arijitbanerjee873@gmail.com>
2025-08-01 23:17:33 +0530

    what

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from "vite";
+import path from "path"; // <-- add this
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // <-- add this
+    },
+  },
 });
